909cf83ee31447eb2529817af34aeda279cd814e
PhamMinhTri26102003 <dh52103871@student.stu.edu.vn>
2024-11-16 10:15:41 +0700

    updatechan

--- file.sh
+++ file.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 echo "Nhap vao 1 so nguyen: "
 read number
-if((number %2 !=0));then
+if((number %2 ==0));then
 	echo "So vua nhap la so chan"
 else 
 	echo "So vua nhap khong la so chan"
